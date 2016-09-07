a17a18c6fcd1ba3175a68c18846947ac53a12a2b
valleysong <valleysong@msn.com>
2016-09-07 17:56:24 +0800

    first sample

---
+++ web/test.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+# import asyncio
+# import web.orm as orm
+# from web.models import User, Blog, Comment
+#
+#
+# async def test(loop):
+#     await orm.create_pool(loop=loop, user='www-data', password='www-data',db='awesome')
+#     u = User(name='Test', email='test@example.com', passwd='1234567890', image='about:blank')
+#     await u.save()
+#     await orm.close_pool()
+#
+# loop = asyncio.get_event_loop()
+# loop.run_until_complete(test(loop))
+# loop.close()
+
+import asyncio
+import orm
+from models import User, Blog, Comment
+
+async def test(loop):
+    await orm.create_pool(loop=loop, user='www-data', password='www-data',db='awesome')
+    u = User(name='Test', email='test@example.com', passwd='1234567890', image='about:blank')
+
+    await u.save()
+    await orm.close_pool()
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(test(loop))
+loop.close()
